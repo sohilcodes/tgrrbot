90a7ba489949ce38d72f60f5bfe65aeb8ba3dc0d
Sohil Khan <96345871+sohilcodes@users.noreply.github.com>
2025-05-27 00:35:29 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    
+    'bot_token' => '7480497994:AAHjosfREzjVOF6s0mkwVLvkrC-N4E2U1-E',
+    
+    'channels' => ['@SohilCodes1', '@SohilScripter', 'https://t.me/+u15M81mT1aU5MDk1'], // Add your channels here
+    
+    'channels_on_check' => ['@SohilCodes',], // Add your channels to check here
+    
+    'bonus_amount' => 2, // Set your bonus amount here
+    
+    'per_reffer_bonus' => 3, // Set your per referral bonus here
+    
+    'min_withdraw_limit' => 10, // Set your minimum withdrawal limit here
+    
+    'f2s_payout_mid' => 'YOUR_F2S_PAYOUT_MID', // Full2sms payout mid
+    
+    'f2s_payout_mkey' => 'YOUR_F2S_PAYOUT_MKEY', // Full2sms payout mkey
+    
+    'f2s_payout_guid' => 'YOUR_F2S_PAYOUT_GUID' // Full2sms payout guid
+];
